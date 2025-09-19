340bff65ceeb655733d01fc9a18d538c74e37e14
Arthur <arthur.mendonca.1352200@sga.pucminas.br>
2025-09-19 20:21:57 -0300

    feat: secure login

--- Código/src/main/java/br/pucminas/lab/aluguelcarros/controller/ClienteController.java
+++ Código/src/main/java/br/pucminas/lab/aluguelcarros/controller/ClienteController.java
@@ -1,11 +1,16 @@
 package br.pucminas.lab.aluguelcarros.controller;
 
-import br.pucminas.lab.aluguelcarros.model.Cliente;
-import br.pucminas.lab.aluguelcarros.service.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.pucminas.lab.aluguelcarros.model.Cliente;
+import br.pucminas.lab.aluguelcarros.service.ClienteService;
 
 @Controller
 @RequestMapping("/clientes")
--- Código/src/test/java/br/pucminas/lab/aluguelcarros/AluguelCarrosApplicationTests.java
+++ Código/src/test/java/br/pucminas/lab/aluguelcarros/AluguelCarrosApplicationTests.java
@@ -3,7 +3,7 @@ package br.pucminas.lab.aluguelcarros;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes={br.pucminas.lab.aluguelcarros.AluguelCarrosApplication.class})
 class AluguelCarrosApplicationTests {
 
 	@Test
